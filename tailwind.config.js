c6de8d354f7c453977a7303721ad195635b19cca
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-02-01 12:03:46 +0200

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes:{
+        'fade-up':{
+         '0%':{
+          opacity: 0,
+          transform: {translateY:'30px', scale: 0.9}
+         },
+         '100%':{
+          opacity: 1,
+          transform: {translateY:'0px', scale: 1}
+         }
+        }
+      },
+    animation:{
+      'fade-up': 'fade-up 2s ease-in-out '
+    }
+    },
+  },
+  plugins: [],
+}
