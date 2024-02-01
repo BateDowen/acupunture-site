19f403091b00feb4a5dc9844492591a40b3e8329
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-02-01 12:04:10 +0200

    create nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="flex">
+    <section className="flex-1 flex flex-row justify-between items-center bg-white fixed w-[100%] shadow-md z-30">
+        <div className="p-10 text-[#48B6BB]" >
+            <img src='../../logo.webp' alt="" height={100} width={100} />
+        </div>
+        <ul className="flex justify-between  min-w-[60%] pr-10 max-sm:hidden">
+            <NavLink to={'/'} className='text-[#747272] hover:text-[#46a5aaf5]'>Начало</NavLink>
+            <NavLink to={'/about'} className='text-[#747272] hover:text-[#46a5aaf5]'>За нас</NavLink>
+            <NavLink to={'/prices'} className='text-[#747272] hover:text-[#46a5aaf5]'>Цени</NavLink>
+            <NavLink to={'/contacts'} className='text-[#747272] hover:text-[#46a5aaf5]'>Контакти</NavLink>
+        </ul>
+        <div className="hidden max-sm:block px-4">
+            <img src='../../public/hamburger.svg' alt="hamb" width={25} height={25} />
+        </div>
+    </section>
+    <Outlet />
+    
+    </div>
+    
+  )
+}
+
+export default Nav;
+
