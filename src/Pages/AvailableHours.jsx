4c5a165cc5579abb96fd56352e9459e0d7377c1d
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-02-18 10:02:30 +0200

    using useffect hook to display hours

--- src/Pages/AvailableHours.jsx
+++ src/Pages/AvailableHours.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Button from "../components/Button/Button";
 import { getDate } from "../Utils";
 
 const AvailableHours =  () => {
   const titleCss = "text-hoverBlue text-2xl sm:text-3xl font-bold mb-6";
-  const [info,setInfo] = useState([])
+  const [info,setInfo] = useState(null)
   const { date } = useParams();
 
-  const response = getDate(date)
-  .then(result => {
-    return Object.values(result.availableHours);
-  })
-  .then(result =>{
-    setInfo(result)
-  })
+  useEffect(() => {
+    const response = getDate(date)
+    .then(result => {
+      return result ? Object.values(result.availableHours) : null;
+    })
+    .then(result =>{
+      console.log(result);
+      setInfo(result)
+    })
+    
+  },[date])
   // TODO loading...
+
     return (
     <div className="bg-[#F5FCFC] relative pt-[100px] w-full mb-0 mx-auto text-center">
       <section className="flex flex-row w-full justify-around mt-[120px] ">
@@ -24,24 +29,25 @@ const AvailableHours =  () => {
         </div>
       </section>
       <div className="text-center flex justify-center h-[600px] bg-hoverBlue">
-        <div className="flex flex-col w-[300px] py-7 justify-between">
+          <div className="flex flex-col md:flex-row max-w-[650px] flex-wrap py-7 justify-around">
           {
-            info.length > 0 ? (
+            info != null ? (
                 info.map((h) => {
-              return (
-                  <div
+                  
+                  return (
+                    <div
                     key={h}
-                    className="w-full h-10 py-2 rounded-md bg-[#F5FCFC]
-                       font-bold text-hoverBlue hover:text-[#ffffff]
-                       hover:bg-lightBlue transition-all ease-linear shadow-customGray"
-                  >
+                    className="w-[300px] h-10 py-2 rounded-md bg-[#F5FCFC]
+                    font-bold text-hoverBlue hover:text-[#ffffff]
+                    hover:bg-lightBlue transition-all ease-linear shadow-customGray"
+                    >
                     {h}
                   </div>
                 );
-            })
-          ) : (
-              <div>
-                <p className="text-[#ffffff] font-medium mb-3">
+              })
+              ) : (
+              <div className="w-full mt-6">
+                <p className="text-[#ffffff] text-lg font-medium mb-3">
                   Няма свободни часове за тази дата
                 </p>
               </div>
