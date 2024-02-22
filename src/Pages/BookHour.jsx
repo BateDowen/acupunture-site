a1ef76a73bed4b539636a1ce9e5d2fa2d5e0ef81
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-02-22 07:10:30 +0200

    create bookHour component

---
+++ src/Pages/BookHour.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Form from '../components/Form/Form';
+import Button from '../components/Button/Button';
+import { bookHour } from '../Utils';
+
+const BookHour = () => {
+    const {date,hour,hourKey } = useParams();
+    console.log(hourKey);
+    const titleCss = "text-hoverBlue text-center text-2xl sm:text-3xl font-bold mb-3";
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        let formData = new FormData(e.currentTarget);
+        let {email,name,phone} = Object.fromEntries(formData);
+      
+         bookHour(email,name,phone,date,hour,hourKey)
+         .then(result => {
+             if (result.err) {
+                 // TODO display it properly
+                 console.log(result.message);
+                };
+            console.log(result);
+                
+         })
+         .catch(err => {
+            console.log(err);
+         })
+    }
+  return (
+    <div className=" bg-white relative pt-[100px] w-full mx-auto text-left">
+      <section className="flex flex-row w-full justify-around mt-[120px] ">
+        <div className={` flex flex-col w-full mb-[30px] mx-12`}>
+          <h2 className={`w-full uppercase ${titleCss} `}>Запази час</h2>
+        </div>
+      </section>
+      <div className="text-center flex justify-center h-[500px] bg-hoverBlue">
+        <div className='mx-auto py-5 w-full'>
+            <div className='my-5'>
+                <Button bg={'btn-primary'}>{hour}</Button>
+
+            </div>
+            <Form onSubmit={onSubmit} bntText='Запази'/>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default BookHour
