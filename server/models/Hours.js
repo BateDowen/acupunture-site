258e3ddef5fed4d70e3338b9f284a0d09868ec11
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-02-22 06:58:33 +0200

    new hoursSchema

---
+++ server/models/Hours.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+// const hoursSchema = new mongoose.Schema({
+//   date: {
+//     type: String,
+//   },
+//   availableHours: [{
+//     type: [String],
+// }
+//   ],
+// });
+
+const hoursSchema = new mongoose.Schema({
+  date: {
+    type: String,
+  },
+  availableHours: {
+    first: { type: String },
+    second: { type: String },
+    third: { type: String },
+    fourth: { type: String },
+    fifth: { type: String },
+    sixth: { type: String },
+    seventh: { type: String },
+    eighth: { type: String },
+  },
+});
+
+export const Hours = mongoose.model("Hours", hoursSchema);
