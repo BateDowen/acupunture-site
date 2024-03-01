316eec7774bbab1d36d7d773700dc9720710374b
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-03-01 10:40:23 +0200

    displaying not available hours with oppacity

--- src/Pages/AvailableHours.jsx
+++ src/Pages/AvailableHours.jsx
@@ -5,30 +5,32 @@ import { getDate } from "../Utils";
 import { useLoading } from "../components/Loader/LoadingCtx";
 import LoaderModal from "../components/Loader/LoaderModal";
 
-const AvailableHours =  () => {
+const AvailableHours = () => {
   const titleCss = "text-hoverBlue text-2xl sm:text-3xl font-bold mb-6";
-  const [availableHours,setAvailableHours] = useState(null)
-  const [availableHoursKeys,setAvailableHoursKeys] = useState(null)
+  const unavailableCss = "cursor-not-allowed opacity-50 bg-red";
+  const [availableHours, setAvailableHours] = useState(null);
+  const [availableHoursKeys, setAvailableHoursKeys] = useState(null);
   const { date } = useParams();
-  const {loading,showLoader,hideLoader} = useLoading()
-  
-  useEffect(() => {
-    showLoader()
-    getDate(date)
-    .then(result => {
-      return [result ? Object.values(result.availableHours) : null, result];
-    })
-    .then(result =>{
-      const keys = Object.keys(result[1].availableHours)
-      setAvailableHoursKeys(keys)
-      setAvailableHours(result[0])
-      hideLoader()
-    })
-    
-  },[date])
+  const { loading, showLoader, hideLoader } = useLoading();
 
-    return (
-      loading? <LoaderModal/>:
+  useEffect(() => {
+    showLoader();
+    getDate(date)
+      .then((result) => {
+        console.log(result);
+        return [result ? Object.values(result.availableHours) : null, result];
+      })
+      .then((result) => {
+        setAvailableHours(result[0]);
+        const keys = Object.keys(result[1].availableHours);
+        setAvailableHoursKeys(keys);
+        hideLoader();
+      });
+  }, [date]);
+
+  return loading ? (
+    <LoaderModal />
+  ) : (
     <div className="bg-[#F5FCFC] relative pt-[100px] w-full mb-0 mx-auto text-center">
       <section className="flex flex-row w-full justify-around mt-[120px] ">
         <div className={` flex flex-col w-full mx-12`}>
@@ -36,41 +38,45 @@ const AvailableHours =  () => {
         </div>
       </section>
       <div className="text-center flex justify-center h-[600px] bg-hoverBlue">
-          <div className="flex flex-col md:flex-row max-w-[650px] flex-wrap py-7 justify-around">
-          {
-            availableHours != null ? (
-                availableHours.map((h,index) => {
-                //  TODO authorize
-                  return (
-                    <Link key={h} to={`/hours/${date}/${availableHoursKeys[index]}/${h}`}>
-                      <div
-                      
-                      className="w-[300px] h-10 py-2 rounded-md bg-[#F5FCFC]
+        <div className="flex flex-col md:flex-row max-w-[650px] flex-wrap py-7 justify-around">
+        
+          {availableHours != null ? (
+            // TODO unavailbleHours red
+          
+            availableHours.map((h, index) => {
+              console.log(h);
+              return (
+                <Link
+                  key={index}
+                  to={`/hours/${date}/${availableHoursKeys[index]}/${h.hour}`}
+                >
+                  <div
+                    className={`${h.available ? '' : unavailableCss} w-[300px] h-10 py-2 rounded-md bg-[#F5FCFC]
                       font-bold text-hoverBlue hover:text-[#ffffff]
-                      hover:bg-lightBlue transition-all ease-linear shadow-customGray"
-                      >
-                      {h}
-                      </div>
-                    </Link>
-                );
-              })
-              ) : (
-              <div className="w-full mt-6">
-                <p className="text-[#ffffff] text-lg font-medium mb-3">
-                  Няма свободни часове за тази дата
-                </p>
-              </div>
-            )}
-          </div>
+                      hover:bg-lightBlue transition-all ease-linear shadow-customGray`}
+                  >
+                    {h.hour}
+                  </div>
+                </Link>
+              );
+            })
+          ) : (
+            
+            <div className="w-full mt-6">
+              <p className="text-[#ffffff] text-lg font-medium mb-3">
+                Няма свободни часове за тази дата
+              </p>
+            </div>
+          )}
         </div>
-        <div className="bg-hoverBlue w-full pb-5">
-        <Link to={"/appointments"}>
-              <Button bg={"btn-primary"}>Назад</Button>
-            </Link>
-        </div>
-           
       </div>
-    );
-  };
-  
+      <div className="bg-hoverBlue w-full pb-5">
+        <Link to={"/appointments"}>
+          <Button bg={"btn-primary"}>Назад</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default AvailableHours;
