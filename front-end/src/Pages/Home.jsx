342f9a764d48e16562e30488939b26560892320e
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-03-07 17:30:01 +0200

    Update Home.jsx

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
             </div>
           </div>
           <img
-            src="'../../../public/massage.jpg"
+            src="'../../public/massage.jpg"
             alt="massage"
             className="mx-4 py-2 rounded-3xl w-[350px] lg:h-[280px] sm:h-[320px]"
           />
@@ -49,10 +49,10 @@ const Home = () => {
           Последни публикации
         </h3>
         <div className="flex flex-row flex-wrap justify-start px-12">
-          <BlogCard href={""} src={"../../../public/massage.jpg"} />
-          <BlogCard href={""} src={"../../../public/massage.jpg"} />
-          <BlogCard href={""} src={"../../../public/massage.jpg"} />
-          <BlogCard href={""} src={"../../../public/massage.jpg"} />
+          <BlogCard href={""} src={"../../public/massage.jpg"} />
+          <BlogCard href={""} src={"../../public/massage.jpg"} />
+          <BlogCard href={""} src={"../../public/massage.jpg"} />
+          <BlogCard href={""} src={"../../public/massage.jpg"} />
         </div>
         <div className="my-2">
           <Button bg="btn-secondary">Виж всички</Button>
