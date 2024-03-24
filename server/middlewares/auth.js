cd38108d545051aa0377be29de068e1b4fc1f3fb
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-03-24 12:04:22 +0200

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,7 +2,6 @@ export const isAdmin = (req,res,next) =>{
     const user = req.body.user ? req.body.user : undefined;
     console.log(req.body);
     if (user) {
-        console.log('fewfqew');
         if (user.name == 'Zlati' && user.role == 'admin') {
             return next()
         } else {
