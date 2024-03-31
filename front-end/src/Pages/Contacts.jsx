085bd4ccf7248311f2ea98693dddf8a683c02000
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2024-03-31 18:03:38 +0300

    change a path to fb

--- front-end/src/Pages/Contacts.jsx
+++ front-end/src/Pages/Contacts.jsx
@@ -43,6 +43,7 @@ const Contacts = () => {
             </p>
             <p>
               <span className=" flex flex-row">
+              <a href='tel:+359893711399' className='text-base mt-2 flex flex-row transition ease-in-out hover:-translate-y-1 hover:scale-125'>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -57,13 +58,14 @@ const Contacts = () => {
                     d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
                   />
                 </svg>
-                <span className="text-base ml-3">+359893711399</span>
+                <span className="text-base ml-3">+359893711399</span></a>
               </span>
             </p>
             <p className="flex flex-row mt-2">
               <a
-                href="https://www.facebook.com/YumeihoBg"
+                href="https://www.facebook.com/MTZlatomirTrifonov"
                 target="_blank"
+                className='transition ease-in-out hover:-translate-y-1 hover:scale-125'
                 rel="noopener noreferrer"
               >
                 <svg
@@ -77,8 +79,9 @@ const Contacts = () => {
                 </svg>
               </a>
               <a
-                href="https://www.facebook.com/YumeihoBg"
+                href="https://www.instagram.com/z.trifonov_massage.therapist"
                 target="_blank"
+                className='transition ease-in-out hover:-translate-y-1 hover:scale-125'
                 rel="noopener noreferrer"
               >
                 <svg
