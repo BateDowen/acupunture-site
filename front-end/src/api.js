30c801114b9e5cf11f97b95dc8f738b10849be2c
Miro1994 <104710043+BateDowen@users.noreply.github.com>
2025-06-13 20:20:13 +0300

    change api

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,5 +1,5 @@
-export const host = 'http://localhost:3030/' ;
-// export const host = 'https://acupunktura.onrender.com/' ;
+// export const host = 'http://localhost:3030/' ;
+export const host = 'https://acupunktura.onrender.com/' ;
 
 const request = async (method,url, data) => {
     const options = {
